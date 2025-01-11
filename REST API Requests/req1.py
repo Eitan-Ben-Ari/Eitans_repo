7f86a9b2c258f4a6778d5b0f7318db52297a8a54
Eitan-Ben-Ari <eitanbenari4@gmail.com>
2025-01-11 13:06:49 +0200

    edit directory and delete old files

--- REST API Requests/req1.py
+++
@@ -1,6 +0,0 @@
-import requests
-uri = "https://netacad.com"
-req1=requests.get(uri, headers={'accept': 'application/json'})
-with open('file.html','w') as file:
-    file.write(f'{req1.content}')
-
